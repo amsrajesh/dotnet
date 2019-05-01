158319359789a1c0aa1e8a4fc4069a689355659c
Michael <amsmicr@gmail.com>
2019-05-01 11:44:03 +0530

    Testing

---
+++ Samples/ConsoleApplications/ConsoleApp1/Box.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Box
+    {
+        public double length;   // Length of a box
+        public double breadth;  // Breadth of a box
+        public double height;   // Height of a box
+    }
+}
